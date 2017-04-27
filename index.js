c7977e7fc4aa1a26c080a074c766b659807daf1f
Zhiqing Qiu <zhqqi@microsoft.com>
2017-04-27 17:10:02 +0800

    add more statistic data

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ var Registry = require('azure-iothub').Registry.fromConnectionString("HostName=z
 var exec = require('child_process').exec;
 var util = require('util');
 var fs = require('fs');
-var cmd = 'docker run -i --rm -v C:/Users/v-zhq/IoT/javatest4/test2:/usr/src/mj -w /usr/src/mj maven mvn compile exec:java -Dexec.args="%s %d"';
+var cmd = 'docker run -i --rm -v C:/Users/v-zhq/.m2:/root/.m2 -v C:/Users/v-zhq/IoT/javatest4/test2:/usr/src/mj -w /usr/src/mj maven:3.5.0-jdk-8 mvn exec:java -Dexec.args="%s %d"'; // remove compile cmd to avoid conflict volume
 
-var NUM = 10;
-var NUM_MESSAGE_PER_DEVICE = 100;
+var NUM_OF_DEVICES = 20;
+var NUM_MESSAGE_PER_DEVICE = 5;
 var stat = {
-    numExec:0,
-    numDone:0,
-    devices:{}, // changed_times,msg_sent,msg_respond
+    numExec: 0,
+    numDone: 0,
+    numFail: 0,
+    changedTimes:{},
+    msgSent:{},
+    msgRespond:{},
+    devices: {}, // changed_times,msg_sent,msg_respond
 };
 
 main();
@@ -24,40 +28,60 @@ function main() {
             deviceList.forEach((device) => {
                 var matches = reg.exec(device.deviceId);
                 if (matches) {
-                    if (parseInt(matches[1]) >= 0 && parseInt(matches[1]) < NUM) {
+                    if (parseInt(matches[1]) >= 0 && parseInt(matches[1]) < NUM_OF_DEVICES) {
                         deviceCS.push({
-                            id:device.deviceId,
-                            cs:"HostName=zhqqi-iot3.azure-devices.net;DeviceId=" + device.deviceId + ";SharedAccessKey=" + device.authentication.symmetricKey.primaryKey
+                            id: device.deviceId,
+                            cs: "HostName=zhqqi-iot3.azure-devices.net;DeviceId=" + device.deviceId + ";SharedAccessKey=" + device.authentication.symmetricKey.primaryKey
                         });
                     }
                 }
             });
-            console.log(util.format("Devices retrieved. (%d/%d)",deviceCS.length,NUM));
-            for (var i = 0; i < NUM; i++) {
-                exec(util.format(cmd,deviceCS[i].cs,NUM_MESSAGE_PER_DEVICE), { maxBuffer: 1024 * 500 }, (function (id, error, stdout, stderr) {
+            console.log(util.format("Devices retrieved. (%d/%d)", deviceCS.length, NUM_OF_DEVICES));
+            for (var i = 0; i < NUM_OF_DEVICES; i++) {
+                exec(util.format(cmd, deviceCS[i].cs, NUM_MESSAGE_PER_DEVICE), { maxBuffer: 1024 * 500 }, (function (id, error, stdout, stderr) {
                     if (error) {
-                        console.log("Error" + error);
+                        console.log("Error with "+ id + "\n" + error);
+                        stat.numDone++;
+                        stat.numFail++;
                     } else {
-                        console.log(util.format("Device %s complete. (%d/%d)",id,++stat.numDone,NUM));
+                        console.log(util.format("Device %s complete. (%d/%d)", id, ++stat.numDone, NUM_OF_DEVICES));
                         stdout = stdout.substring(stdout.indexOf('Starting...'));
-                        var m1 = stdout.match(/Sampling rate changed to/g);
-                        var m2 = stdout.match(/Event Message \d+/g);
-                        var m3 = stdout.match(/IoT Hub responded to message \d+/g);
-                        if(!stat.devices[id]) {
-                            stat.devices[id] = {};
-                        }
-                        stat.devices[id].changed_times = ( m1 == null ? 0 : m1.length);
-                        stat.devices[id].msg_sent = ( m2 == null ? 0 : m2.length);
-                        stat.devices[id].msg_respond = ( m3 == null ? 0 : m3.length);
-                        fs.writeFile("log/"+id+".log",stdout,()=>{});
-                        if(stat.numDone == NUM) {
-                            //finished
-                            console.log(stat);
-                            fs.writeFile("log/main.log",stat,()=>{});
-                        }
+
                     }
-                }).bind(this,deviceCS[i].id));
-                console.log(util.format("Device %s started. (%d/%d)",deviceCS[i].id,++stat.numExec,NUM));
+                    var m1 = stdout.match(/Sampling rate changed to/g);
+                    var m2 = stdout.match(/Event Message \d+/g);
+                    var m3 = stdout.match(/IoT Hub responded to message \d+/g);
+                    if (!stat.devices[id]) {
+                        stat.devices[id] = {};
+                    }
+                    stat.devices[id].changed_times = (m1 == null ? 0 : m1.length);
+                    stat.devices[id].msg_sent = (m2 == null ? 0 : m2.length);
+                    stat.devices[id].msg_respond = (m3 == null ? 0 : m3.length);
+                    fs.writeFile("log/" + id + ".log", stdout, () => { });
+                    if (stat.numDone == NUM_OF_DEVICES) {
+                        //finished
+                        for(var d in stat.devices) {
+                            if(! stat.changedTimes[stat.devices[d].changed_times]) {
+                                stat.changedTimes[stat.devices[d].changed_times] = 1;
+                            }else {
+                                stat.changedTimes[stat.devices[d].changed_times]++; 
+                            }
+                            if(! stat.msgSent[stat.devices[d].msg_sent]) {
+                                stat.msgSent[stat.devices[d].msg_sent] = 1;
+                            }else {
+                                stat.msgSent[stat.devices[d].msg_sent]++; 
+                            }
+                            if(! stat.msgRespond[stat.devices[d].msg_respond]) {
+                                stat.msgRespond[stat.devices[d].msg_respond] = 1;
+                            }else {
+                                stat.msgRespond[stat.devices[d].msg_respond]++; 
+                            }
+                        }
+                        console.log(stat);
+                        fs.writeFile("log/main.log", util.inspect(stat), () => { });
+                    }
+                }).bind(this, deviceCS[i].id));
+                console.log(util.format("Device %s started. (%d/%d)", deviceCS[i].id, ++stat.numExec, NUM_OF_DEVICES));
             }
         }
     });
